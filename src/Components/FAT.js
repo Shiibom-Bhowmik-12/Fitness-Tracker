8d84a76e831883e1574119a1e2485a6bbe262026
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-05-22 17:02:30 +0530

    Update FAT.js

--- src/Components/FAT.js
+++ src/Components/FAT.js
@@ -7,84 +7,95 @@ function FAT() {
   const [height,setHeight]=useState('');
   const [gender,setGender]=useState('');
   const [age,setAge]=useState('');
-  const [report,setReport]=useState('');
   const [percentage,setPercentage]=useState('');
 
 
   const CalcFat = (e) => {
     e.preventDefault();
     
+    let bfp=0;
     if(weight === '' || height === '' || gender === '' ||age === '') {
       toaster.danger('Please enter all the details!');
     }
     else{
       toaster.success('BFP calculated successfully!');
       let bmi = ((weight/height)/height)*10000;
-      let bfp=0;
-      if(gender==='Male' && (age>0 && age<=18)){
-        bfp=(1.51*bmi)-(0.70*age)-2.2;
+      if(gender==='Male' || gender==='Male ')
+      {
+          if(age>0 && age<=18){
+            bfp=(1.51*bmi)-(0.70*age)-2.2;
+            setPercentage(bfp.toFixed(1));
+          }
+          else{
+            bfp=(1.20*bmi)+(0.23*age)-16.2;
+            setPercentage(bfp.toFixed(1));
+          }
       }
-      else if(gender==='Female' && (age>0 && age<=18)){
-        bfp=(1.51*bmi)-(0.70*age)-1.4;
+      else if(gender==='Female' || gender==='Female '){
+        if(age>0 && age<=18){
+          bfp=(1.51*bmi)-(0.70*age)-1.4;
+          setPercentage(bfp.toFixed(1));
+        }
+        else{
+          bfp=(1.20*bmi)+(0.23*age)-5.4;
+          setPercentage(bfp.toFixed(1));
+        }
       }
-      else if(gender==='Male' && age>18){
-        bfp=(1.20*bmi)+(0.23*age)-16.2;
-      }
-      else if(gender==='Female' && age>18){
-        bfp=(1.20*bmi)+(0.23*age)-5.4;
-      }
-
-      setPercentage(bfp.toFixed(1));
-    }
-
-    if((gender==="Female" && (percentage>=10 && percentage<=13))){
-      setReport('Your Body Fat Category is - "Essential fat"');
-    }
-    else if((gender==="Male" && (percentage>=2 && percentage<=5))){
-      setReport('Your Body Fat Category is - "Essential fat"');
-    }
-    else if((gender==="Female" && (percentage>=14 && percentage<=20))){
-      setReport('Your Body Fat Category is - "Athletes"');
-    }
-    else if((gender==="Male" && (percentage>=6 && percentage<=13))){
-      setReport('Your Body Fat Category is - "Athletes"');
-    }
-    else if((gender==="Female" && (percentage>=21 && percentage<=24))){
-      setReport('Your Body Fat Category is - "Fitness"');
-    }
-    else if((gender==="Male" && (percentage>=14 && percentage<=17))){
-      setReport('Your Body Fat Category is - "Fitness"');
-    }
-    else if((gender==="Female" && (percentage>=25 && percentage<=31))){
-      setReport('Your Body Fat Category is - "Average"');
-    }
-    else if((gender==="Male" && (percentage>=18 && percentage<=24))){
-      setReport('Your Body Fat Category is - "Average"');
-    }
-    else if((gender==="Female" && percentage>=32)){
-      setReport('Your Body Fat Category is - "Obese"');
-    }
-    else if((gender==="Male" && percentage>24)){
-      setReport('Your Body Fat Category is - "Obese"');
     }
   }
 
-  let imgSrc;
-  if(percentage<1){
-    imgSrc = null;
+
+  let msg;
+  if(percentage>0 && percentage<=5){
+    msg='“To change your body you must first change your mind.”';
+  }
+  else if(percentage>5 && percentage<=10){
+    msg='“Working out is never convenient. But neither is illness, diabetes and obesity!”';
+  }
+  else if(percentage>10 && percentage<=15){
+    msg='“When it burns, is when you’re just getting started. That’s when you get stronger!”';
+  }
+  else if(percentage>15 && percentage<=20){
+    msg='“Think of your workouts as important meetings you’ve scheduled with yourself. Bosses don’t cancel.”';
+  }
+  else if(percentage>20 && percentage<=25){
+    msg='“You must tell yourself, no matter how hard it is, or how hard it gets, I’m going to make it.”';
+  }
+  else if(percentage>25 && percentage<=30){
+    msg='“Results happen over time, not overnight. Work hard, stay consistent and be patient.”';
+  }
+  else if(percentage>30 && percentage<=35){
+    msg='“Motivation is what gets you started. Habit is what keeps you going.”';
+  }
+  else if(percentage>35 && percentage<=40){
+    msg='“Don’t compare yourself to others. Compare yourself to the person from yesterday.”';
   }
   else{
+    msg='“Nothing tastes as good as being thin feels.”';
+  }  
+
+
+
+
+  let imgSrc;
+  if(percentage === ''){
+    imgSrc=null;
+    msg='';
+  }
+  else if(percentage>1){
     imgSrc = require('../Components/assetss/bfp4.png');
   }
+
+
 
   let reload = () => {
     setWeight('');
     setHeight('');
     setGender('');
     setAge('');
-    setReport('');
     setPercentage('');
     imgSrc=null;
+    msg='';
   }
 
   return (
@@ -116,17 +127,19 @@ function FAT() {
                 <button className='btn btn-outline' onClick={reload} type='reload'>Reset</button>
                   <div className='center'>
                       <h3>Your Body Fat Percentage is : {percentage} %</h3>
-                      <p>{report}</p>
+                      <p>{msg}</p>
                   </div>
+                  <br/>
                   <div className='img-container'>
                     <img src={imgSrc} alt=''></img>
                   </div>
+                  <br/>
                 <footer className='footer'>
                     <h4>Made with <span style={{color:"red"}}>&hearts;</span> by Shibom</h4>
                 </footer>
           </div>
         </div>
     )
-  }
+}
 
 export default FAT;
